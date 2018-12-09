d1d04b85104ed775d38bab544db3afcc1931d1a6
Viktoriia Stasiuk <32536332+viktoriiaStasiuk@users.noreply.github.com>
2018-12-09 20:58:18 +0100

    Add files via upload

---
+++ assets/images/kenny_platformer_64x64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="kenny_platformer_64x64" tilewidth="64" tileheight="64" tilecount="0" columns="5">
+ <image source="kenny_platformer_64x64.png" width="320" height="1024"/>
+</tileset>
---
+++ assets/tilemaps/RPGpack_sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="RPGpack_sheet" tilewidth="64" tileheight="64" tilecount="260" columns="20">
+ <image source="../images/RPGpack_sheet.png" width="1280" height="832"/>
+</tileset>
